796f9ee3f07e3f3ece1dd0262e50f33c5895d1d1
edisonrees <130874746+edisonrees@users.noreply.github.com>
2024-03-06 21:36:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 /**
- ██████╗░████████╗██╗░░██╗           
- ██╔══██╗╚══██╔══╝╚██╗██╔╝          
- ██████╔╝░░░██║░░░░╚███╔╝░          
- ██╔══██╗░░░██║░░░░██╔██╗░          
- ██║░░██║░░░██║░░░██╔╝╚██╗          
- ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-  GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
-  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
  * **********************************************
- *   Code by RTX GAMING
+ *   Code by Edison Rees
  * **********************************************
  */
 
@@ -31,8 +22,9 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+console.log(`Copy 200 Listening On localhost:${port}`);
+  console.log(`Data Valued 200`);
+
 });
 
 
@@ -45,7 +37,7 @@ const channelId = '';
 async function login() {
   try {
     await client.login(process.env.TOKEN);
-    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+    console.log(`\x1b[36m%s\x1b[0m`, `|     Verified as ${client.user.tag`);
   } catch (error) {
     console.error('Failed to log in:', error);
     process.exit(1);
@@ -53,17 +45,8 @@ async function login() {
 }
 
 /**
- ██████╗░████████╗██╗░░██╗           
- ██╔══██╗╚══██╔══╝╚██╗██╔╝          
- ██████╔╝░░░██║░░░░╚███╔╝░          
- ██╔══██╗░░░██║░░░░██╔██╗░          
- ██║░░██║░░░██║░░░██╔╝╚██╗          
- ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
-  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
  * **********************************************
- *   Code by RTX GAMING
+ *   Code by Edison Rees
  * **********************************************
  */
 
@@ -92,8 +75,22 @@ function updateStatusAndSendMessages() {
 
 client.once('ready', () => {
   console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Ready. Running Params Check`);
+console.log(`\x1b[36m%s\x1b[0m`, `|    Verified Params, Checking Gateway Access`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Verified Gateway, Checking Rich Content Injections `);
+ console.log(`\x1b[36m%s\x1b[0m`, `|     Rich Content Injections Failed, Purging Injections `);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Injections Purged, Retrying Injections 401 400  `);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Verified Injection , Status Code 200 302 Found`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    ERROR, RICH PRESENCE CANNOT BE VERIFIED Status Codes 401 400 404 Retrying `);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Verified Rich Prescence, Checking Validity Of Console 101 Switching Protocols`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Final Check, Status 200, Params 200, Injections 200, Console 200 101, Gateway 200`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Setup Confimed.`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Completing...`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    511 Network Authentication Required`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Authentication Waiting.... Success`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    All Systems Code 200, OK`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Deploying Cache Load`);
+ console.log(`\x1b[36m%s\x1b[0m`, `|    Deploying Completed. System Operational.`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
@@ -104,16 +101,7 @@ client.once('ready', () => {
 login();
 
 /**
- ██████╗░████████╗██╗░░██╗           
- ██╔══██╗╚══██╔══╝╚██╗██╔╝          
- ██████╔╝░░░██║░░░░╚███╔╝░          
- ██╔══██╗░░░██║░░░░██╔██╗░          
- ██║░░██║░░░██║░░░██╔╝╚██╗          
- ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
-  DISCORD SERVER : https://discord.gg/FUEHs7RCqz
-  YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
  * **********************************************
- *   Code by RTX GAMING
+ *   Code by Edison Rees
  * **********************************************
  */
